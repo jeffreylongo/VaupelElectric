d6bd718ffbbe66ca4c172e578cd88b8822c4fb1a
Jeff Longo <jeffreylongo@yahoo.com>
2017-06-27 09:50:36 -0400

    Generate files

---
+++ VaupelElectric/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VaupelElectric.Startup))]
+namespace VaupelElectric
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
